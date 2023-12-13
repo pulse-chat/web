f182f072d070789339afea9767b4efdb787045e9
WorriedArrow <worriedarrow@outlook.com>
2023-12-13 15:32:26 -0800

    yeah

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Pulse';
+export const SITE_DESCRIPTION = 'Your new chat platform';
